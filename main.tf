8e6e43926e74566f5dc7cfcfaa2a6e6185f88070
tabotayuk <151395059+tabotayuk@users.noreply.github.com>
2023-11-24 20:37:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
 
+provider "aws" {
+  region = "us-east-1"  # Update with your desired AWS region
+}
+
+resource "aws_instance" "example_instance" {
+  ami           = "ami-0230bd60aa48260c6"  # Replace with the desired AMI ID
+  instance_type = "t2.micro"      # Replace with the desired instance type
+
+  tags = {
+    Name = "ExampleInstance"  # Replace with your preferred instance name
+  }
+}
+
+extra line 
